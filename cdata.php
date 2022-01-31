de863192ff10cea60f49b60fca5aa66e046c2592
Felipe Mancilla <felipei.mancilla@gmail.com>
2022-01-31 12:32:16 -0300

    ADD contacto

--- cdata.php
+++ cdata.php
@@ -6,6 +6,7 @@ require_once('zklib/ZKLib.php');
 /*
  * @author: Felipe Mancilla
  * Date: 2022/01/25
+ * web: http://felipemancilla.herokuapp.com/
  * @description: se recibe data por get con la ip y serial del dispositivo, este archivo debe ser ejecutado por un CronJob o tarea programada
  * se debe considerar un cronjob por cada reloj y este debe ser ejecutado con un maximo de 1 segundo (idealmente 0.5 seg).
  * @exec : ejemplo de ejecución  manual: http://localhost/iclock/cdata?ip=10.6.17.216
